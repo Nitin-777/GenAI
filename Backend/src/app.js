885ecd142c0dff22c390cf265cc44d9770b3c44a
Nitin <GenAi>
2026-06-22 23:43:01 +0530

    api error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ const app= express();
 app.use(express.json()) 
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://prepwizeai.netlify.app/login",
+  origin: "https://prepwizeai.netlify.app",
   credentials: true
 }));
 const authRouter= require('./routes/auth.routes')
